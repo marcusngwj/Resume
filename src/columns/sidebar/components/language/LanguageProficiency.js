e183ae4665541503c357bc3aedef1bb189b9ae2c
marcusngwj <marcusngwj@msn.com>
2018-12-08 20:29:32 +0800

    update language

--- src/columns/sidebar/components/language/LanguageProficiency.js
+++ src/columns/sidebar/components/language/LanguageProficiency.js
@@ -11,9 +11,26 @@ export default class LanguageProficiency extends Component {
     return (
       <div className="language-proficiency">
         <p className="title">Language Proficiency</p>
-        <p className="content">English</p>
-        <p className="content">Chinese</p>
-        <p className="content">Japanese</p>
+        <div className="content">
+          <div className="language">
+            <p className="text">English</p>
+            <div className="proficiency">
+              <span className="shaded"></span><span className="shaded"></span><span className="shaded"></span><span className="shaded"></span><span className="shaded"></span>
+            </div>
+          </div>
+          <div className="language">
+            <p className="text">Chinese</p>
+            <div className="proficiency">
+              <span className="shaded"></span><span className="shaded"></span><span className="shaded"></span><span className="shaded"></span><span className="shaded"></span>
+            </div>
+          </div>
+          <div className="language">
+            <p className="text">Japanese</p>
+            <div className="proficiency">
+              <span className="shaded"></span><span className="unshaded"></span><span className="unshaded"></span><span className="unshaded"></span><span className="unshaded"></span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
