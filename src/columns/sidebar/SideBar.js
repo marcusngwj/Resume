2a92cbe657b8ab8572d69ea3332375ba47581bfc
marcusngwj <marcusngwj@msn.com>
2018-12-08 21:34:37 +0800

    update programming skills

--- src/columns/sidebar/SideBar.js
+++ src/columns/sidebar/SideBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import PersonalData from './components/personal_data/PersonalData';
 import LanguageProficiency from './components/language/LanguageProficiency';
+import ProgrammingSkills from './components/programming_skills/ProgrammingSkills';
 
 export default class SideBar extends Component {
   // constructor(props) {
@@ -13,6 +14,7 @@ export default class SideBar extends Component {
       <div className="sidebar">
         <PersonalData />
         <LanguageProficiency />
+        <ProgrammingSkills />
       </div>
     );
   }
---
+++ src/columns/sidebar/components/programming_skills/ProgrammingSkills.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+export default class ProgrammingSkills extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="programming-skills">
+        <p className="title">Programming Skill Sets</p>
+        <div className="content">
+          <table>
+            <tr>
+              <td>Java</td>
+              <td>Python</td>
+            </tr>
+            <tr>
+              <td>C++</td>
+              <td>C</td>
+            </tr>
+            <tr>
+              <td>Objective C</td>
+              <td>JavaScript</td>
+            </tr>
+            <tr>
+              <td>HTML, CSS</td>
+              <td>SCSS</td>
+            </tr>
+            <tr>
+              <td>React.js</td>
+              <td>PHP</td>
+            </tr>
+            <tr>
+              <td>PostgreSQL</td>
+              <td>MySQLi</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
