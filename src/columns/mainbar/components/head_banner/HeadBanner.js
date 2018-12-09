42cbc48e917b7d68aeea204c7c5b015276a8d7c8
marcusngwj <marcusngwj@msn.com>
2018-12-09 14:26:11 +0800

    update summary in header

--- src/columns/mainbar/components/head_banner/HeadBanner.js
+++ src/columns/mainbar/components/head_banner/HeadBanner.js
@@ -20,14 +20,15 @@ export default class HeadBanner extends Component {
               <td className="content">
                 <p id="fullname">Marcus Ng Wen Jian</p>
                 <p className="text">Final Year NUS Computer Science Undergraduate</p>
-                <p className="text">BComputing (Honours) in Computer Science program</p>
+                <p className="text">BComputing (Honours) in Computer Science</p>
                 <p className="text"><img src={droppin} /><span>Singapore</span></p>
               </td>
             </tr>
             <tr>
               <td colspan="2">
                 <div className="description">
-                  Hardworking individual with experience in Software Engineering, focusing in Mobile Development. Adaptable team player with problem-solving skills
+                  Highly motivated and hardworking individual with diverse experience.<br />
+                  Adaptable and trustworthy team player with problem-solving skills.
                 </div>
               </td>
             </tr>
